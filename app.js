f9155eb659a9f9d0072e8f3107a8e763ae346bd3
Ross Clettenberg <raclettenberg@gmail.com>
2022-09-15 21:15:26 -0500

    Add CSS. Add hamburger menu. Add header and footer. Pasted in individual pages for each section. Delete  bibliography page and link. Make bibliography the footer. Elaborate on conversation starter facts. Add another remix. Test app.

--- app.js
+++ app.js
@@ -10,12 +10,31 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.render("origins");
 });
 
+app.get("/origins", function(req, res){
+  res.render("origins");
+});
 
+app.get("/making-it", function(req, res){
+  res.render("making-it");
+});
 
+app.get("/serving-it", function(req, res){
+  res.render("serving-it");
+});
+
+app.get("/remixes", function(req, res){
+  res.render("remixes");
+});
+
+app.get("/conversation-starters", function(req, res){
+  res.render("conversation-starters");
+});
 
 
 app.listen(process.env.PORT || 3000, function () {
